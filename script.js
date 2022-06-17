c94bd5258c437477806c2f5b8b52a40c61669896
RaghRoog <szumon612@interia.pl>
2022-06-18 01:25:50 +0200

    (almost)done

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = document.getElementById('password')
+let confirmPassword = document.getElementById('confirm-password')
+let submitButton = document.querySelector('.submit')
+let firstname = document.getElementById('first-name')
+let notMatch = document.querySelector('.not-match')
+
+
